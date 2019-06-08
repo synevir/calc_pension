4ebe7bcd7306c9f844cf5d95860f8e98a70fd8cc
reebok <you@example.com>
2019-06-08 23:29:09 +0300

    Impoved function `worst_range` Now it works with zerro sequence properly

--- worst_range.rb
+++ worst_range.rb
@@ -5,19 +5,22 @@ class Array
 end
 
 def worst_range(a, percent = 10)
-#    frame_size = a.size * percent / 100
+# frame_size = a.size * percent / 100
 # If you want to change `percent` value uncoment row above and comment row under
-   frame_size = 3
+  frame_size = 3
+  total_sum = a.sum
+  total_cnt = a.count
   [].tap do |result|
     a.each_with_index do |i, ind|
       (1..[a.slice(ind, frame_size).size, frame_size].min).each do |c|
+        frame = a.slice(ind, c)
         result << {
           start: ind,
           cnt: c,
-          items: a.slice(ind, c),
-          value: a.slice(ind, c).mean * (a.size - c) / c
+          items: frame,
+          value: ((total_sum - frame.sum).to_f / (total_cnt - c)).round(5)
         }
       end
     end 
-  end.min { |a, b| a[:value] <=> b[:value] }
+  end.max { |a, b| a[:value] <=> b[:value] }
 end
